0c52b7ac9fd553145740f936e55c8f7145e7aabc
samedwardcar <106223288+samedwardcar@users.noreply.github.com>
2022-10-04 07:42:52 -0500

    read me

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const app = express()
 
 app.use('/places', require('./controllers/places'))
 
+// GET /places
 app.get('/', (req, res) => {
-    res.send('Hello world!')
-})
+    let places = []
+    res.render('places/index')
+  })
+  
 
 app.get('*', (req, res) => {
     res.status(404).send('<h1>404 Page</h1>')
